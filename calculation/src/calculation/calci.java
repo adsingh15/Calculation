46d52b6bdaf79b4d2d6a72f82d0d9a505fdf9a19
adsingh15 <aakkashsingh22@gmail.com>
2022-08-10 15:56:20 +0530

    commit

---
+++ calculation/src/calculation/calci.java
@@ -0,0 +1,13 @@
+package calculation;
+
+public class calci {
+
+	public static void main(String[] args) {
+		int a=10; int b=5;
+		int sum= a+b;
+		int sub= a-b;
+		System.out.println(sum);
+		System.out.println(sub);
+	}
+
+}
